f0472a0652b59f149b802963394f5938a650f161
OlegS <xwx18@mail.ru>
2019-04-19 18:57:19 +0300

    добавил offline

---
+++ src/provider.js
@@ -0,0 +1,52 @@
+import Adapter from '../src/data-adapter.js';
+
+const createArrayFromObjectData = (object) => {
+  return Object.keys(object).map((id) => object[id]);
+};
+
+export default class Provider {
+  constructor({api, store}) {
+    this._api = api;
+    this._store = store;
+    this._needSync = false;
+  }
+
+  getData() {
+    if (this._isOnline()) {
+      return this._api.getData()
+        .then((data) => {
+          data.map((it) => this._store.setItem({key: it.id, item: Adapter.toRAW(it)}));
+          return data;
+        });
+    } else {
+      const rawDataMap = this._store.getAll();
+      const rawData = createArrayFromObjectData(rawDataMap);
+      const data = Adapter.parseData(rawData);
+
+      return Promise.resolve(data);
+    }
+  }
+
+  updateData({id, newData}) {
+    if (this._isOnline()) {
+      return this._api.updateData({id, newData})
+        .then((datum) => {
+          this._store.setItem({key: datum.id, item: Adapter.toRAW(datum)});
+          return datum;
+        });
+    } else {
+      const datum = newData;
+      this._needSync = true;
+      this._store.setItem({key: datum.id, item: datum});
+      return Promise.resolve(Adapter.parseDatum(datum));
+    }
+  }
+
+  syncData() {
+    return this._api.syncData({data: createArrayFromObjectData(this._store.getAll())});
+  }
+
+  _isOnline() {
+    return window.navigator.onLine;
+  }
+}
